7255e1a35282468215377944b7340e6ee433f201
Arundhati <arundhatiraicar12@gmail.com>
2023-08-20 14:31:24 -0400

    Implemented post and delete reqs

--- src/components/MyJournal.jsx
+++ src/components/MyJournal.jsx
@@ -5,7 +5,45 @@ import TripCard from "./TripCard";
 
 function MyJournal() {
    const[trips,setTrips]=useState([]);
- 
+   const [formData,setFormData]=useState({
+      destination:'',
+      date:'',
+      budget:'',
+      accomodation:'',
+      notes:''
+   })
+
+   function handleChange(e){
+      setFormData({...formData, [e.target.name]: e.target.value})
+   }
+   function handleSubmit(e) {
+      e.preventDefault();
+  
+      const newTrip ={  ...formData}
+        
+    
+      axios.post('http://localhost:3000/trips', newTrip)
+      .then(function (response) {
+        console.log("Response data:", response.data);
+        const newTripData = response.data;
+
+        setTrips([...trips, newTripData]);
+
+      // Clear the form fields
+      setFormData({
+        destination: '',
+        date: '',
+        budget: '',
+        accomodation: '',
+        notes: ''
+      });
+      })
+      .catch( (error)=> {
+         console.log(error);
+      });
+        
+    }
+    
    async function getTrips() {
       try {
          const response = await axios.get("http://localhost:3000/trips");
@@ -19,14 +57,20 @@ function MyJournal() {
    useEffect(() => {
       getTrips();
     }, []); 
-  
+
+    function handleDelete(id) {
+      setTrips(trips.filter(trip => trip.id !== id));
+    }
   return (
+<>
     <div className="flex  flex-wrap justify-center pl-1  h-screen">
-      <div className="w-2/4 max-sm:w-[80%] max-sm:ml-[10%]  py-3">
+      <div className="w-2/4   py-3">
         <h1 className="text-lg font-semibold text-center mt-0 p-1">Plan your trip...🗺️</h1>
 
-        <div className="w-4/5 mx-auto  flex flex-col justify-center items-center p-3 ">
-          <div className="w-full">
+        <div className="w-4/5 mx-auto  justify-center items-center p-3 ">
+        <form>
+          <div className="">
+         
             <label htmlFor="destination" className="block text-sm font-medium text-gray-900">
               Destination
             </label>
@@ -34,6 +78,7 @@ function MyJournal() {
               type="text"
               name="destination"
               id="destination"
+              value={formData.destination} onChange={handleChange} 
               className="w-full h-8 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
@@ -44,8 +89,9 @@ function MyJournal() {
             </label>
             <input
               type="date"
-              name="departureDate"
-              id="departureDate"
+              name="date"
+              id="date"
+              value={formData.date} onChange={handleChange} 
               className="w-full h-8 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
@@ -58,6 +104,7 @@ function MyJournal() {
               type="text"
               name="budget"
               id="budget"
+              value={formData.budget} onChange={handleChange} 
               className="w-full h-8 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
@@ -68,8 +115,10 @@ function MyJournal() {
             </label>
             <input
               type="text"
-              name="accommodation"
-              id="accommodation"
+              name="accomodation"
+              id="accomodation"
+            
+              value={formData.accomodation} onChange={handleChange} 
               className="w-full h-8 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
@@ -79,9 +128,10 @@ function MyJournal() {
               Activities and Notes
             </label>
             <textarea
-              name="activitiesAndNotes"
-              id="activitiesAndNotes"
+              name="notes"
+              id="notes"
               rows="3"
+              value={formData.notes} onChange={handleChange} 
               className="w-full h-20 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             ></textarea>
           </div>
@@ -89,29 +139,35 @@ function MyJournal() {
           <div className="w-full mt-3">
             <button
               type="submit"
-              className="bg-blue-600 text-white px-4 py-2 rounded-md"
+              className="bg-blue-600 text-white px-4 py-2 rounded-md" onClick={handleSubmit}
             >
               Add
             </button>
+           
+            
           </div>
+          </form>
         </div>
+       
       </div>
 
       
       <div className="w-8.5  py-3 w-2/4 max-sm:w-[80%] max-sm:ml-[10%]  h-305 ">
      
-        <h1 className="text-lg font-semibold text-center overflow-auto  p-1">My Trips</h1>
-          <div className="w-4/5  xs:w-full ">
+        <h1 className="text-lg font-semibold text-center    p-1">My Trips</h1>
+          <div className="w-4/5  xs:w-full h-4/6 overflow-auto ">
            
           
            {trips.map((trip, i) => (
-    <TripCard trip={trip} key={i} />
+    <TripCard  trip={trip} key={i} onDelete={handleDelete} />
   ))}
          
         </div>
       </div>
     </div>
+    </>
   );
+ 
 }
 
 export default MyJournal;
--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,12 +1,24 @@
 import "../index.css"
-function TripCard({trip,i}){
+import axios from "axios";
+function TripCard({trip, onDelete}){
+    function deleteCard(){
+        axios.delete(`http://localhost:3000/trips/${trip.id}`)
+  .then(response => {
+    onDelete(trip.id)
+  })
+  .catch(error => {
+    console.error(error);
+  });
+    }
     return(
         <>
-         <div className=" bg-custom-gradient justify-center  text-white rounded  ml-16  px-7   items-start p-2">
+        
+         <div className=" bg-custom-gradient justify-center mt-1 text-white rounded  ml-16  px-7   items-start p-2">
+         <button onClick={deleteCard} className="bg-blue-600 rounded-sm w-5" h-3>X</button>
         <h1>Trip: {trip.id}</h1>
         <h1>Name :  {trip.destination}</h1>
         <h1>Date: {trip.date}</h1>
-        <h1>Budget : {trip.budget}</h1>
+        <h1>Budget : ${trip.budget}</h1>
         <h1>Acomodation : {trip.accomodation}</h1>
         <h1>Activites and Notes : {trip.notes}</h1>
         </div>
